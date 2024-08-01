6fd8d5b45dabbdc8f4e61e759a403057c64db90a
Stephen John De Julian <dejuliansm.109.stud@cdd.edu.ph>
2024-08-01 16:10:13 +0800

    First commit

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -11,7 +11,7 @@ integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6
 <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-warning">
     <div class="container-fluid">
-      <a class="navbar-brand h1" href={{ route('posts.index') }}>CRUDPosts</a>
+      <a class="navbar-brand h1" href={{ route('posts.index') }}>damn</a>
       <div class="justify-end ">
         <div class="col ">
           <a class="btn btn-sm btn-success" href={{ route('posts.create') }}>Add Post</a>
